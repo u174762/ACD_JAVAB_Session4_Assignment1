c199eb9c32e0e95c88810617fa12dc785499c04d
u174762 <sam.panga@hotmail.com>
2016-08-08 00:07:40 +0530

    Add files via upload

---
+++ ACD_JAVA_session4-assignment1/src/assignment1.java
@@ -0,0 +1,18 @@
+public class assignment1 {
+
+			 public static void main(String[] args) {
+				 int arrnum [] = new int []{12,10,100,67,90}; 
+				// print the original array 
+				System.out.println("printing original array"); 
+				
+				for (int i=0;i<arrnum.length;i++){ 
+				
+					System.out.println(arrnum[i]); 
+				} 
+				// print in reverse 
+				
+		      System.out.println("Printing reverse array");
+		      
+		      for(int i = arrnum.length - 1; i >=0;i--){
+		    	  System.out.println(arrnum[i]);
+		      }}}
